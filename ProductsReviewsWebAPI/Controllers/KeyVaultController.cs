e9e770ae3694d12b79cc912aa27992c5854e266d
Megan Cindric <megancindric@gmail.com>
2023-09-18 10:53:07 -0600

    📦 NEW: Adding test KV Secret endpoint

---
+++ ProductsReviewsWebAPI/Controllers/KeyVaultController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductsReviewsWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KeyVaultController : ControllerBase
+    {
+        // GET: api/<KeyVaultController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+        {
+            Delay= TimeSpan.FromSeconds(2),
+            MaxDelay = TimeSpan.FromSeconds(16),
+            MaxRetries = 5,
+            Mode = RetryMode.Exponential
+         }
+            };
+            var client = new SecretClient(new Uri("https://keyvault-test-mac.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+            KeyVaultSecret secret = client.GetSecret("Message");
+
+            string secretValue = secret.Value;
+            return Ok(secretValue);
+        }
+
+        // GET api/<KeyVaultController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<KeyVaultController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<KeyVaultController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<KeyVaultController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
